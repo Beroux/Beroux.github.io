d7d3a95e4b3283edb6ff822cd649a1a09745195c
Beroux <31445450+Beroux@users.noreply.github.com>
2017-10-10 06:54:07 -0400

    Initial commit

---
+++ Assets/Scripts/Bork.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bork : MonoBehaviour {
+    public Collider Player;
+    public Collider bork;
+    public AudioClip bjork;
+    public AudioSource nyan;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            nyan.PlayOneShot(bjork, 0.5f);
+
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# Beroux.github.io
