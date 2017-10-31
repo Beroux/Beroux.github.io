1da97c357e0c3116facb4ac68100ea698a3995e0
Beroux <31445450+Beroux@users.noreply.github.com>
2017-10-30 23:06:51 -0400

    Added: Prototype to the Final Project folder
    Controls for two players, Will impliment four in the future and obstacles, but this is a proof of concept at the moment.

---
+++ Final Project GD 205/Assets/Scripts/Player2.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player2 : MonoBehaviour {
+
+    Rigidbody rb;
+    public float thrustAmt;
+
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody> ();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate () {
+        if (Input.GetKey (KeyCode.I)) {
+            rb.AddForce (Vector3.forward * thrustAmt);
+        }
+        if (Input.GetKey (KeyCode.K)) {
+            rb.AddForce (Vector3.back * thrustAmt);
+        }
+        if (Input.GetKey (KeyCode.J)) {
+            rb.AddForce (Vector3.left * thrustAmt);
+        }
+        if (Input.GetKey (KeyCode.L)) {
+            rb.AddForce (Vector3.right * thrustAmt);
+        }
+    }
+}
