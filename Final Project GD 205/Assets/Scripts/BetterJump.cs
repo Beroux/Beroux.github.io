292dbd03e677818c6aaca94f0980572753de8a31
Beroux <31445450+Beroux@users.noreply.github.com>
2017-11-14 10:39:05 -0500

    Update 2, Enemies
    Enemies are functioning as planned, Next step: A spawner, capping the infinite jump and expanding the play area.

---
+++ Final Project GD 205/Assets/Scripts/BetterJump.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BetterJump : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    [Range(1, 10)]
+    public float jumpVelocity;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump2"))
+        {
+            GetComponent<Rigidbody>().velocity = Vector3.up * jumpVelocity;
+        }
+    }
+}
---
+++ Final Project GD 205/Assets/Scripts/Seek.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Seek : MonoBehaviour {
+    public Transform target;
+    Rigidbody seeker;
+    public float thrust;
+    public int secs2wait = 5;
+    public int[] totalSec = new int[5];
+
+
+           
+    // Use this for initialization
+    void Start()
+    {
+        InvokeRepeating("TimerInvoke", 1, 1);
+        seeker = GetComponent<Rigidbody>();
+    }
+
+    void TimerInvoke()
+    {
+       // if (totalSec[0] < secs2wait)
+         //   totalSec[0]++;
+       // else
+         //   CancelInvoke("TimerInvoke");
+        Vector3 targetDir = Vector3.Normalize(target.position - transform.position);
+        seeker.AddForce(targetDir * thrust);
+    }
+
+
+// Update is called once per frame
+void Update () {
+        
+
+    }
+}
